aa1c8f6b0f070a23ecbacf90749be76906d87513
Iury <iury.santos@cognizant.com>
2018-04-25 15:42:20 -0300

    Add AutoIncrement Argument

--- biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/model/Emprestimo.java
+++ biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/model/Emprestimo.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -16,7 +17,7 @@ public class Emprestimo implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id",nullable=false,updatable=false)
 	private Long id;
 	
