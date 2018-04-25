337faea65bfd6a6360704b2138218b64d6ab1772
Iury <iury.santos@cognizant.com>
2018-04-25 16:15:26 -0300

    alteration Autor class (equals())

--- biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/model/Autor.java
+++ biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/model/Autor.java
@@ -35,50 +35,10 @@ public class Autor implements Serializable {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((livros == null) ? 0 : livros.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Livro other = (Livro) obj;
-		if (anoPublicacao != other.anoPublicacao)
-			return false;
-		if (edicao != other.edicao)
-			return false;
-		if (foto == null) {
-			if (other.foto != null)
-				return false;
-		} else if (!foto.equals(other.foto))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (isbn13 == null) {
-			if (other.isbn13 != null)
-				return false;
-		} else if (!isbn13.equals(other.isbn13))
-			return false;
-		if (sinopse == null) {
-			if (other.sinopse != null)
-				return false;
-		} else if (!sinopse.equals(other.sinopse))
-			return false;
-		if (titulo == null) {
-			if (other.titulo != null)
-				return false;
-		} else if (!titulo.equals(other.titulo))
-			return false;
-		return true;
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -93,6 +53,11 @@ public class Autor implements Serializable {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
+			return false;
+		if (livros == null) {
+			if (other.livros != null)
+				return false;
+		} else if (!livros.equals(other.livros))
 			return false;
 		if (nome == null) {
 			if (other.nome != null)
