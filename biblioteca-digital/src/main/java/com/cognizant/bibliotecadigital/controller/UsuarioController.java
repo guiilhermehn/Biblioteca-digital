3e2c31116ad3e27db13e5395aef587f7eddc6e02
brunojsan <brunojsan@hotmail.com>
2018-04-27 15:20:18 -0300

    Visualização de detalhes de usuarios

--- biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/controller/UsuarioController.java
+++ biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,8 +35,28 @@ public class UsuarioController {
 	@PostMapping(path = "/usuarios/salvar")
 	public ModelAndView create(@ModelAttribute Usuario usuario) {
 		usuarioService.save(usuario);
-		return new ModelAndView("redirect:/usuarios");
-		
+		return new ModelAndView("redirect:/usuarios");		
 	}
+	
+	
+	@GetMapping("/usuarios/{id}")
+	public ModelAndView detail(@PathVariable("id") Long id) {
+		ModelAndView mv = new ModelAndView("/usuario/usuario");
+		mv.addObject("usuario",usuarioService.findById(id));
+		
+		return mv;
+	}
+	
+	
+	/* Alteração Bruno TODO : Usuario altera seu perfil ( dados pessoais)
+	@GetMapping("/usuarios/perfil")
+	public ModelAndView novo() {
+	        ModelAndView modelAndView = new ModelAndView("/pessoa/pessoa_form");
+	        modelAndView.addObject("usuario", new Usuario());
+	        return modelAndView;
+	}	
+	*/
+	
+	
 
 }
