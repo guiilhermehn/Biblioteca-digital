a967fd1e7547267a3d724ec0d913c579be76a636
Diego Matsuki <diego.matsuki@gmail.com>
2018-04-28 17:47:26 -0300

    Organizando o código

--- biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/repository/LivroRepository.java
+++ biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/repository/LivroRepository.java
@@ -1,8 +1,5 @@
 package com.cognizant.bibliotecadigital.repository;
 
-
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
