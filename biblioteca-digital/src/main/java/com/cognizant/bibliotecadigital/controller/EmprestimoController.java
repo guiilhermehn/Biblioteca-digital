65b48394f2b8a657d5f2032a35237b2d9f6c65d9
psousaraquel <38658746+psousaraquel@users.noreply.github.com>
2018-05-03 10:28:27 -0300

    Emprestimo

---
+++ biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/controller/EmprestimoController.java
@@ -0,0 +1,34 @@
+package com.cognizant.bibliotecadigital.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cognizant.bibliotecadigital.service.EmprestimoService;
+
+@Controller
+public class EmprestimoController {
+
+	@Autowired
+	private EmprestimoService emprestimoService;
+
+	@GetMapping("/emprestimos")
+	public ModelAndView findAll() {
+		ModelAndView mv = new ModelAndView("/emprestimos/emprestimos");
+		mv.addObject("emprestimos", emprestimoService.findAll());
+
+		return mv;
+	}
+	
+	@PostMapping("/emprestimos/deletarEmprestimo")
+	public ModelAndView deletar(@RequestParam("id") Long id) {
+		emprestimoService.deleteById(id);
+		ModelAndView mv = new ModelAndView("redirect:/emprestimos");		
+		
+		return mv;
+	}
+}
