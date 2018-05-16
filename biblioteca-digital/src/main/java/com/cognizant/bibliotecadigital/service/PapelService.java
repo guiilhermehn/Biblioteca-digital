0245e4d31ae53c40d77ad809597771c860c523d1
Jackson França <bassline_knight@hotmail.com>
2018-05-16 16:17:26 -0300

    Correção Pesquisa

--- biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/service/PapelService.java
+++ biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/service/PapelService.java
@@ -1,8 +1,11 @@
 package com.cognizant.bibliotecadigital.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cognizant.bibliotecadigital.model.Papel;
 import com.cognizant.bibliotecadigital.repository.PapelRepository;
 
 @Service
@@ -11,7 +14,7 @@ public class PapelService {
 	@Autowired
 	private PapelRepository papelRepository;
 	
-	public Long findByNome(String nome) {
+	public Optional<Papel> findByNome(String nome) {
 		return papelRepository.findByNome(nome);
 	}
 }
