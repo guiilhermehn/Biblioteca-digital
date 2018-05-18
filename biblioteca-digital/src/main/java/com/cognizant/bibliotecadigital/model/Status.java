fdac4ce7ff86ece0edd5cd0e60881918040eef93
DiegoMatsuki <diego.matsuki@gmail.com>
2018-05-18 14:35:32 -0300

    lalalala

--- biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/model/Status.java
+++ biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/model/Status.java
@@ -2,5 +2,22 @@ package com.cognizant.bibliotecadigital.model;
 
 public enum Status {
 
-	EM_ESPERA,AGUARDANDO,FINALIZADO,EM_ANALISE;
+	EM_ESPERA, AGUARDANDO, FINALIZADO, EM_ANALISE;
+	
+	@Override
+	public String toString() {
+		if (this == EM_ESPERA ) {
+			return "Em espera";
+		}
+		if (this == AGUARDANDO ) {
+			return "Aguardando";
+		}	
+		if (this == FINALIZADO ) {
+			return "Finalizado";
+		}	
+		if (this == EM_ANALISE ) {
+			return "Em análise";
+		}	
+		return "Não encontrado";
+	}
 }
