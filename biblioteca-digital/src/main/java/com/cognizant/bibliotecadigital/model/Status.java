11b4171cde0ff22d008a24f486934b1ae382e61b
Guilherme Henrique Nascimento <guilherme_hn@icloud.com>
2018-05-21 15:06:52 -0300

    Correção Status Reserva

--- biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/model/Status.java
+++ biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/model/Status.java
@@ -5,26 +5,25 @@ public enum Status {
 
 
 	EM_ESPERA, AGUARDANDO, FINALIZADO, EM_ANALISE,ATIVO;
-	
+
 	@Override
 	public String toString() {
 		if (this == EM_ESPERA ) {
-			return "Em espera";
+			return "Em Espera";
 		}
 		if (this == AGUARDANDO ) {
 			return "Aguardando";
-		}	
+		}
 		if (this == FINALIZADO ) {
 			return "Finalizado";
-		}	
+		}
 		if (this == EM_ANALISE ) {
-			return "Em análise";
+			return "Em Análise";
 		}
 		if (this == ATIVO ) {
 			return "Ativo";
-		}	
+		}
 		return "Não encontrado";
 	}
 
 }
-
