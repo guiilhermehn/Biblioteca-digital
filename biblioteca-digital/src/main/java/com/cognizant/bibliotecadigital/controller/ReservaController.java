8e97998478a3f17c8cec29c434c149b9e4b463e0
philipe-silva <38658934+philipe-silva@users.noreply.github.com>
2018-05-21 13:40:14 -0300

    Commit

--- biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/controller/ReservaController.java
+++ biblioteca-digital/src/main/java/com/cognizant/bibliotecadigital/controller/ReservaController.java
@@ -6,9 +6,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.mail.MessagingException;
 
